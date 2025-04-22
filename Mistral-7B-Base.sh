1851259e48e4a73b28b6b2dfa942f390bd0f5468
tengxiao1 <53974866+tengxiao1@users.noreply.github.com>
2025-04-22 10:55:20 -0400

    submit

---
+++ Mistral-7B-Base.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=3,4,5,7
+
+iter_num=15
+for i in $(seq 1 $iter_num); do
+    output_dir="models/$hub_model_id" # mode save local path
+    gradient_accumulation_steps=16
+    per_device_train_batch_size=2
+    if [ "$i" -eq 1 ]; then
+        beta=1.0
+        learning_rate=5e-7
+        hub_model_id="Mistral-7B-Base-LSIF-5e-7"
+        loss_type="LSIF"
+        output_dir="models/$hub_model_id"
+        ACCELERATE_LOG_LEVEL=info  accelerate launch --config_file recipes/accelerate_configs/deepspeed_zero3.yaml scripts/run.py recipes/zephyr/mistral-7b-base-simpo.yaml run_name=$hub_model_id  learning_rate=$learning_rate hub_model_id=$hub_model_id output_dir=$output_dir loss_type=$loss_type  beta=$beta
+    elif [ "$i" -eq 2 ]; then
+        beta=1.0
+        learning_rate=1e-6
+        hub_model_id="Mistral-7B-Base-LSIF-1e-6"
+        loss_type="LSIF"
+        output_dir="models/$hub_model_id"
+        ACCELERATE_LOG_LEVEL=info  accelerate launch --config_file recipes/accelerate_configs/deepspeed_zero3.yaml scripts/run.py recipes/zephyr/mistral-7b-base-simpo.yaml run_name=$hub_model_id  learning_rate=$learning_rate hub_model_id=$hub_model_id output_dir=$output_dir loss_type=$loss_type  beta=$beta
+    elif [ "$i" -eq 3 ]; then
+        beta=1.0
+        learning_rate=6e-7
+        hub_model_id="Mistral-7B-Base-LSIF-6e-7"
+        loss_type="LSIF"
+        output_dir="models/$hub_model_id"
+        ACCELERATE_LOG_LEVEL=info  accelerate launch --config_file recipes/accelerate_configs/deepspeed_zero3.yaml scripts/run.py recipes/zephyr/mistral-7b-base-simpo.yaml run_name=$hub_model_id  learning_rate=$learning_rate hub_model_id=$hub_model_id output_dir=$output_dir loss_type=$loss_type  beta=$beta
+    elif [ "$i" -eq 4 ]; then
+        beta=1.0
+        learning_rate=3e-7
+        hub_model_id="Mistral-7B-Base-LSIF-3e-7"
+        loss_type="LSIF"
+        output_dir="models/$hub_model_id"
+        ACCELERATE_LOG_LEVEL=info  accelerate launch --config_file recipes/accelerate_configs/deepspeed_zero3.yaml scripts/run.py recipes/zephyr/mistral-7b-base-simpo.yaml run_name=$hub_model_id learning_rate=$learning_rate hub_model_id=$hub_model_id output_dir=$output_dir loss_type=$loss_type  beta=$beta
+    else
+       continue 
+    fi
+    echo "Training"
+
+
+done
+
+
---
+++ README.md
@@ -0,0 +1,43 @@
+## Direct Imitation Learning: RLHF Secretly Performs Imitation Learning
+
+This repository contains the code for the ICLR 2025 submission [Direct Imitation Learning: RLHF Secretly Performs Imitation Learning](https://openreview.net/forum?id=2QdsjiNXgj). 
+
+
+### Environment
+
+First, install PyTorch `2.1.2` from the [PyTorch Installation Page](https://pytorch.org/get-started/locally/).
+
+Create a Python virtual environment using e.g. Conda:
+
+```shell
+conda create -n DIL python=3.10 && conda activate DIL
+```
+
+```shell
+python -m pip install flash-attn --no-build-isolation
+```
+
+## Training Scripts
+
+
+* Mistral-Base:
+```shell
+bash Mistral-7B-Base.sh
+```
+
+* Llama3-Base:
+```shell
+bash Llama-8B-Base.sh
+```
+
+
+## Evaluation Benchmarks
+
+
+* [Open LLM Leadboard v1](https://huggingface.co/spaces/open-llm-leaderboard-old/open_llm_leaderboard)
+
+* [Open LLM Leadboard v2](https://huggingface.co/spaces/open-llm-leaderboard/open_llm_leaderboard) 
+
+
+
+
